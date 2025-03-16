8155ded2cb89cf0a9229847dbe5ad1818280f48e
iniad <cornforprogramming@gmail.com>
2025-03-16 14:42:07 +0900

    マイグレーション時にトランザクション利用するように設定変更

--- tunetracker_backend/settings.py
+++ tunetracker_backend/settings.py
@@ -145,3 +145,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MIGRATION_MODULES = {
+    'default': {
+        'atomic': True,
+    },
+}
